c8f48b990be27c9bf35725fccf6d1392c7f1c861
Abdul Moiz <abdulmoiz3140@gmail.com>
2024-08-18 17:53:31 +0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,29 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Learn MAte",
-  description: "Generated your flashcard - Gemini",
-};
-
-export default function RootLayout({ children }) {
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import './globals.css'
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
-        {children}
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
         </body>
       </html>
     </ClerkProvider>
-  );
+  )
 }
