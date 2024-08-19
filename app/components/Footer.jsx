a9eb7cb122cadae8ac29bb50700c38b341fc0df6
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-08-19 22:12:52 +0500

    code

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <footer className="bg-gray-800 text-center text-white p-3  w-full">
+        <footer className="bg-gray-800 text-center text-white p-3 relative bottom-0  w-full">
             <p>© 2021 All Rights Reserved</p>
         </footer>
 
