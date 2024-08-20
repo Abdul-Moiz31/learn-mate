1818cba8a3e81f166b96d7c8d264edfc89b4bfb6
Abdul Moiz <abdulmoiz3140@gmail.com>
2024-08-20 18:45:02 +0500

    Update Loader.jsx

--- app/components/Loader.jsx
+++ app/components/Loader.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2" 
+           style={{ borderTopColor: '#936DFF', borderBottomColor: '#936DFF' }}>
+      </div>
     </div>
   )
 }
